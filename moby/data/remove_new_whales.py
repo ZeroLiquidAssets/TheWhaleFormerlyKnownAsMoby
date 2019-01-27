ff06652e7e3e0d29d1cc51fb63ee69a584a9e2f6
Thomas Tu <thomasthetu@gmail.com>
2019-01-26 17:27:27 -0800

    bug fixes and linting
    Former-commit-id: dbe1c21bab8cea3892fec6d9141f20a02f533490 [formerly ce9df74dc59d5922355a740dcc0e133618c7da18]
    Former-commit-id: 5e8ee41066c6ff25ba1aa75cea0c57fa8bd3cfa6

--- moby/data/remove_new_whales.py
+++ moby/data/remove_new_whales.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import pandas as pd
 
 from logzero import logger
@@ -6,14 +7,24 @@ from logzero import logger
 from moby.constants import DATA_DIR
 
 
+logger.setLevel(logging.INFO)
+
+
 def main(output_dir):
+    """Re-create train.csv without new_whale, and creates a copy of all non-new_whale images.
+    
+    Args:
+        output_dir (pathlib.Path): pathlib.Path object to the target directory.
+    """
+    output_dir.mkdir(parents=True, exist_ok=True)
+
     train = pd.read_csv(DATA_DIR / "raw" / "train.csv")
-    raw_train_images = pd.read_csv(DATA_DIR / "raw" / "train")
+    raw_train_images = DATA_DIR / "raw" / "train"
 
     # Remove "new_whale"
     new_whales = train.query('Id == "new_whale"')
     train_no_new_whales = train.drop(new_whales.index)
-    
+
     n_diff = train.shape[0] - train_no_new_whales.shape[0]
 
     # Write the training file
@@ -21,20 +32,27 @@ def main(output_dir):
 
     # Move images
     output_img_dir = output_dir / "train"
+    output_img_dir.mkdir(parents=True, exist_ok=True)
+
     for img in train_no_new_whales["Image"].unique():
         src = raw_train_images / img
         target = output_img_dir / img
-        logger.debug(
-            "Copying %(src)s to %(target)s", 
-            {"src": src, "target", target}
-        )
-        os.copy(src, target)
+        # logger.debug("Linking %(src)s to %(target)s", {"src": src, "target": target})
+        os.symlink(src, target)
     logger.info("Removed %(n)d 'new_whale' from train.csv", {"n": n_diff})
-    logger.info("Copied %(n)d non-'new_whale' images from %(src)s to %(target)s",
-        {"n": train_no_new_whales.shape[0], "src": raw_train_images, "target": output_img_dir}
+    logger.info(
+        "Linked %(n)d non-'new_whale' images from %(src)s to %(target)s",
+        {
+            "n": train_no_new_whales.shape[0],
+            "src": raw_train_images,
+            "target": output_img_dir,
+        },
     )
 
 
+default_output_dir = DATA_DIR / "interim" / "1_no_new_whales"
+
 if __name__ == "__main__":
-    output_dir = DATA_DIR / "interim" / "1_no_new_whales"    
-    main(output_dir)
+    """Script mode for development.
+    """
+    main(default_output_dir)
